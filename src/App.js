d01df2e2604ab844b8f233c43eeca9b54fdbed71
divyatharshinia <divyaanand2310@gmail.com>
2024-06-25 23:06:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/saga-blue/theme.css'; // Import theme
+import 'primereact/resources/primereact.min.css';         // Core CSS
+import 'primeicons/primeicons.css';                       // Icons
+import 'primeflex/primeflex.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    return(
+    <div>
+        <header className='head'>
+          <h1>Prime React</h1>
+          <Button label="click me" icon="pi pi-check" />
+        </header>
     </div>
-  );
+    )
 }
-
 export default App;
